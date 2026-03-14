92adcbb923a47f4aaafea29bb192d0f09c8a2948
JessicaIrai <64023630+JessicaIrai@users.noreply.github.com>
2026-03-14 08:40:08 -0600

    Update main.py
    Funciones especiales

--- Unidad II/proyecto_biblioteca/main.py
+++ Unidad II/proyecto_biblioteca/main.py
@@ -3,6 +3,7 @@ from modelos.usuario import Usuario
 from servicios.gestorPrestamos import GestorPrestamos
 
 libro1 = Libro("Pepa Pig", "Bucanero", "12345")
+libro2 = Libro("Teoría de Evolución del Ser Humano", "Jorge el curioso", "12345")
 usuario1 = Usuario("Valeria", "2521578")
 
 gestor = GestorPrestamos()
@@ -11,6 +12,10 @@ mensaje = gestor.realizar_prestamo(libro1, usuario1, "2026-03-07")
 
 print(mensaje)
 
-print(libro1.disponibilidad)
+print(libro1.getDisponibilidad())
 
 gestor.listar_prestamos()
+
+print(libro1.getTitulo());
+
+print(libro1 == libro2)
